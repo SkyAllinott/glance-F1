54a3ecf69daf369a321f913395c1a9bd78f2cb92
SkyAllinott <skyallinott@gmail.com>
2025-06-25 21:58:51 -0600

    Remove too fast pit stops based on modified IQR approach, needs refinement

--- API/API_Endpoints/pit_results.py
+++ API/API_Endpoints/pit_results.py
@@ -39,14 +39,42 @@ for session in session_key:
 
     pit_stops_df = pd.DataFrame.from_dict(pit_stops)
 
-    
+    # Calculate mean duration for pit time for a given lap. 
+    # This helps sort out red flags or retirements. 
     pits = pit_stops_df.groupby('lap_number', as_index=False).agg(
         pitstop_mean = ('pit_duration', 'mean'),
         pitstop_min = ('pit_duration', 'min'),
         drivers_in = ('driver_number', 'count')
     )
 
-    average = pit_stops_df.groupby('driver_number', as_index=False).agg(
+    # Use a modified IQR to define outliers. We only use it to remove outliers that are too
+    # fast. Outliers which are too slow generally aren't bad data but a bad pit stop. 
+
+    # Use pretty close to median for low side of estimation range.
+    # The pit stops are generally only 2-3 seconds, so if they're driving through they
+    # are very close to median
+    pitstops_low_quantile = pits['pitstop_mean'].quantile(0.5)
+
+    # A very long pit stop generally isn't an issue
+    pitstops_high_quantile = pits['pitstop_mean'].quantile(0.95)
+    iqr = pitstops_high_quantile-pitstops_low_quantile
+
+    lower_bound = pitstops_low_quantile - 1.5*iqr
+
+    pit_stops_df_filtered = pit_stops_df[(pit_stops_df['pit_duration'] > lower_bound)]
+
+    total_stops = len(pit_stops_df)
+    valid_stops = len(pit_stops_df_filtered)
+    laps_removed = total_stops-valid_stops
+
+    drivers_data = requests.get('https://api.openf1.org/v1/drivers?session_key=' + str(session)).json()
+    drivernumber_name_dict = {drivers['driver_number']: drivers['last_name'] for drivers in drivers_data}
+    drivernumber_team_dict = {drivers['driver_number']: drivers['team_name'] for drivers in drivers_data}
+
+    pit_stops_df_filtered['name'] = pit_stops_df_filtered['driver_number'].map(drivernumber_name_dict)
+    pit_stops_df_filtered['teamName'] = pit_stops_df_filtered['driver_number'].map(drivernumber_team_dict)
+
+    average = pit_stops_df_filtered.groupby(['driver_number', 'name', 'teamName'], as_index=False).agg(
         pitstop_mean = ('pit_duration', 'mean'), 
         pitstop_min = ('pit_duration', 'min')
     )
@@ -54,22 +82,17 @@ for session in session_key:
 
     average['race'] = [x for x in session_info if x["session_key"]== session][0]['circuit_short_name']
 
-    cols = ['pitstop_mean', 'pitstop_min']
+    cols = ['pitstop_mean', 'pitstop_min', 'name']
     average['rank'] = average.sort_values(cols, ascending=True).groupby(cols, sort=False).ngroup() + 1
 
     average['points'] = average['rank'].map(points).fillna(0)
-
-    drivers_data = requests.get('https://api.openf1.org/v1/drivers?session_key=' + str(session)).json()
-    drivernumber_name_dict = {drivers['driver_number']: drivers['last_name'] for drivers in drivers_data}
-    drivernumber_team_dict = {drivers['driver_number']: drivers['team_name'] for drivers in drivers_data}
-
-    average['name'] = average['driver_number'].map(drivernumber_name_dict)
-    average['teamName'] = average['driver_number'].map(drivernumber_team_dict)
 
     average = average.sort_values(by='rank')
     data.append(average)
 
 df = pd.concat(data, ignore_index=True)
+
+print(df.sort_values('race').to_string())
 
 team_results = df.groupby('teamName', as_index=False).agg(
     points = ('points', 'sum')
